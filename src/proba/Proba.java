0706a54d966215c357515a96eae1dffc8a0a61fb
evingerbono <bono0823@gmail.com>
2023-10-02 09:37:23 +0200

    keverés_javítás

--- src/proba/Proba.java
+++ src/proba/Proba.java
@@ -13,6 +13,7 @@ public class Proba {
 
     private static void jatek() {
         pakliKirak();
+        kiiratas(pakli);
         for (int i = 0; i < 3; i++) {
             keveres(bekeres());
         }
@@ -21,30 +22,21 @@ public class Proba {
 
     private static void pakliKirak() {
         String[] szinek = {"P", "Z", "T", "M"};
-        String[] ertek = {"Alsó", "Felső", "Ász","VIII", "IX", "X"};
+        String[] ertek = {"Alsó", "Felső", "Ász", "VIII", "IX", "X"};
         for (int i = 0; i < pakli.length; i++) {
-            pakli[i] = szinek[i/6] + "-" + ertek[i%6];
-        }
-        int db = 0;
-        for (int i = 0; i < pakli.length; i++) {
-            System.out.printf(pakli[i], "  ");
-            db++;
-            if (db == 3) {
-                System.out.println("");
-                db = 0;
-            }
+            pakli[i] = szinek[i / 6] + "-" + ertek[i % 6];
         }
     }
 
     private static int bekeres() {
-        Scanner sc = new Scanner(System .in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Melyik oszlopba van a kártyád(1-3):");
         int bekeres = sc.nextInt();
         return bekeres;
     }
 
     private static void keveres(int oszlop) {
-        String[] ujPakli = new String[21];
+        String[] ujPakli = new String[22];
         switch (oszlop) {
             case 1:
                 for (int i = 0; i < 7; i++) {
@@ -52,6 +44,7 @@ public class Proba {
                     ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
                     ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
                 }
+                kiiratas(ujPakli);
                 break;
             case 2:
                 for (int i = 0; i < 7; i++) {
@@ -59,6 +52,7 @@ public class Proba {
                     ujPakli[i + 7] = pakli[21 - (i - 1) * 3];
                     ujPakli[i + 14] = pakli[19 - (i - 1) * 3];
                 }
+                kiiratas(ujPakli);
                 break;
             case 3:
                 for (int i = 0; i < 7; i++) {
@@ -66,12 +60,27 @@ public class Proba {
                     ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
                     ujPakli[i + 14] = pakli[19 - (i - 1) * 3];
                 }
+                kiiratas(ujPakli);
                 break;
         }
+        pakli=ujPakli;
     }
 
     private static void eredmenyKiiras() {
-        System.out.printf("A kiválasztott lapod:%d",pakli[11]);
+        System.out.printf("A kiválasztott lapod:%d", pakli[11]);
+    }
+
+    private static void kiiratas(String[] pakli) {
+        int db = 0;
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.printf(pakli[i]);
+            System.out.print("    ");
+            db++;
+            if (db == 3) {
+                System.out.println("");
+                db = 0;
+            }
+        }
     }
 
 }

6538dccf400cdef160a0ff5f2f49af4f619c43f8
evingerbono <bono0823@gmail.com>
2023-10-02 09:13:53 +0200

    metodusok_létrehozása_és_pakli_összerakása

--- src/proba/Proba.java
+++ src/proba/Proba.java
@@ -1,20 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proba;
 
-/**
- *
- * @author evinger.bono
- */
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Proba {
 
-    /**
-     * @param args the command line arguments
-     */
+    static String[] pakli = new String[22];
+    static Scanner sc = new Scanner(InputStream.nullInputStream());
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        jatek();
     }
-    
+
+    private static void jatek() {
+        pakliKirak();
+        for (int i = 0; i < 3; i++) {
+            keveres(bekeres());
+        }
+        eredmenyKiiras();
+    }
+
+    private static void pakliKirak() {
+        String[] szinek = {"P", "Z", "T", "M"};
+        String[] ertek = {"VIII", "IX", "X", "Alsó", "Felső", "Ász"};
+        for (int i = 1; i < pakli.length; i++) {
+            pakli[i] = szinek[i] + "-" + ertek[i];
+        }
+        int db = 0;
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.printf(pakli[i], "  ");
+            db++;
+            if (db == 3) {
+                System.out.println("");
+                db = 0;
+            }
+        }
+    }
+
+    private static int bekeres() {
+        System.out.println("Melyik oszlopba van a kártyád(1-3):");
+        int bekeres = sc.nextInt();
+        return bekeres;
+    }
+
+    private static void keveres(int oszlop) {
+        String[] ujPakli = new String[22];
+        switch (oszlop) {
+            case 1:
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i]=pakli[20-(i-1)*3];
+                    ujPakli[i+7]=pakli[19-(i-1)*3];
+                    ujPakli[i+14]=pakli[21-(i-1)*3];
+                }
+                break;
+            case 2:
+
+                break;
+            case 3:
+                
+                break;
+        }
+    }
+
+    private static void eredmenyKiiras() {
+
+    }
+
 }
